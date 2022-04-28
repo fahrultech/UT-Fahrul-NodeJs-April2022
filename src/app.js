26dd2adb902f535fc971cdc18ebd65fff1071534
fahrulglintsassessment <fahrulglintsassesment@gmail.com>
2022-04-28 22:34:52 +0700

    finish swagger setting

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const morgan = require('morgan');
-const { protect, admin } = require('./middleware/authMiddleware')
+const { protect, admin } = require('./middleware/authMiddleware');
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocument = require('./swagger.json')
 
 const api = require('./app/api')
 const app = express();
 
 app.use(morgan('combined'));
 app.use(express.json());
-app.use('/api/v1', api)
+app.use('/api/v1', api);
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 
 module.exports = app;
